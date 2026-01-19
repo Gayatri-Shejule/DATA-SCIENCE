51eb3f5a376fc5320e77d7c92850b09cec683c35
Gayatri Shejule <gayatrishejule454@gmail.com>
2026-01-19 16:41:15 +0530

    math

---
+++ PYTHON/13_math.py
@@ -0,0 +1,17 @@
+import math
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+a = abs(-7.25)
+print(a)
+x = pow(4, 3)
+print(x)
+x = math.sqrt(64)
+print(x)
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x) # returns 2
+print(y) # returns 
+x = math.pi
+print(x)
