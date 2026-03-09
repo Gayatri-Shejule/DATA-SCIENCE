8c76fbd31d178550d526de10dc521a6a3af5bb55
Gayatri Shejule <gayatrishejule454@gmail.com>
2026-03-10 04:28:06 +0530

    pandas data cleaning

--- PYTHON/45_data_cleaning.py
+++ PYTHON/45_data_cleaning.py
@@ -7,3 +7,21 @@ print(df)
 new_df = df.dropna()
 
 print(new_df.to_string())
+
+df['Date'] = pd.to_datetime(df['Date'], format='mixed')
+
+print(df.to_string())
+
+df['Date'] = pd.to_datetime(df['Date'], format='mixed')
+
+print(df.to_string())
+
+for x in df.index:
+  if df.loc[x, "Duration"] > 120:
+    df.drop(x, inplace = True)
+
+print(df.to_string())
+
+df.drop_duplicates(inplace = True)
+
+print(df.to_string())
